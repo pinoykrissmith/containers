f4795b0da064e79fa4a5a10b8dddf5ee9e4f03be
Kris <mfcc_kalman@slmail.me>
2025-01-17 16:32:43 +0000

    Pipe requirements.txt

--- ebook2audiobookpiper/docker-entrypoint.sh
+++ ebook2audiobookpiper/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 if [ -z "$(ls -A /app/)" ]; then
-    git clone --depth 1 https://github.com/DrewThomasson/ebook2audiobookpiper-tts.git /app/
-    sed -i 's/demo\.launch(share=True)/demo.launch(server_name="0.0.0.0", server_port=7860)/' /app/gradio_gui.py
-    python Auto_download_all_piper_models.py
+    git clone --depth 1 https://github.com/DrewThomasson/ebook2audiobookpiper-tts.git .
+    sed -i 's/demo\.launch(share=True)/demo.launch(server_name="0.0.0.0", server_port=7860)/' gradio_gui.py
 fi
 exec "$@"
