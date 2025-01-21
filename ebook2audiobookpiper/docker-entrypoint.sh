bc6c3c6eb40b83e08163569b0b4e2c38d2162fe8
Kris <mfcc_kalman@slmail.me>
2025-01-21 18:29:54 +0000

    Changed some ports to 8080

--- ebook2audiobookpiper/docker-entrypoint.sh
+++ ebook2audiobookpiper/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$(ls -A /app/)" ]; then
     git clone --depth 1 https://github.com/DrewThomasson/ebook2audiobookpiper-tts.git .
-    sed -i 's/demo\.launch(share=True)/demo.launch(server_name="0.0.0.0", server_port=7860)/' gradio_gui.py
+    sed -i 's/demo\.launch(share=True)/demo.launch(server_name="0.0.0.0", server_port=8080)/' gradio_gui.py
 fi
 exec "$@"
