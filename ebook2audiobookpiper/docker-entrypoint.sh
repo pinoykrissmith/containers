a71e7fea360357742404230d11df8e5b17858071
Kris <mfcc_kalman@slmail.me>
2025-01-22 19:41:52 +0000

    Updated ebook2audiobookpiper

--- ebook2audiobookpiper/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -z "$(ls -A /app/)" ]; then
-    git clone --depth 1 https://github.com/DrewThomasson/ebook2audiobookpiper-tts.git .
-    sed -i 's/demo\.launch(share=True)/demo.launch(server_name="0.0.0.0", server_port=8080)/' gradio_gui.py
-fi
-exec "$@"
