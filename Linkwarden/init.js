5661a6a5ddb08dd470620127bd0a0dc2776266eb
Kris <mfcc_kalman@slmail.me>
2025-02-25 23:33:17 +0000

    Updated linkwarden dockerfile

--- Linkwarden/init.js
+++ Linkwarden/init.js
@@ -1,14 +1,34 @@
 const { spawn } = require('child_process');
-function runCommand(command, args) {
-	const child = spawn(command, args, { stdio: 'inherit', shell: false });
+const commands = [
+	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/prisma', 'migrate', 'deploy'] },
+	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/next', 'start'] },
+	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/ts-node', '--transpile-only', '--skip-project', 'scripts/worker.ts'] }
+];
 
-	child.on('error', (err) => {
-		console.error(`Error starting process: ${err.message}`);
+const exec = (bin, args) => {
+	return new Promise((resolve, reject) => {
+		const process = spawn(bin, args, { stdio: 'inherit' });
+		process.on('error', (err) => {
+			reject(`Failed to start process: ${err.message}`);
+		});
+		process.on('close', (code) => {
+			if (code !== 0) {
+				reject(`Command failed with exit code ${code}`);
+			} else {
+				resolve();
+			}
+		});
 	});
+};
 
-	child.on('exit', (code) => {
-		console.log(`Process exited with code: ${code}`);
-	});
-}
-runCommand('/nodejs/bin/node', ['node_modules/.bin/next', 'start']);
-runCommand('/nodejs/bin/node', ['node_modules/.bin/ts-node', '--transpile-only', '--skip-project', 'scripts/worker.ts']);
+const start = async () => {
+	try {
+		await exec(commands[0].bin, commands[0].args);
+		const nextProcess = spawn(commands[1].bin, commands[1].args, { stdio: 'inherit', detached: true });
+		nextProcess.unref();
+		await exec(commands[2].bin, commands[2].args);
+	} catch (error) {
+		console.error('Error:', error);
+	}
+};
+start();
