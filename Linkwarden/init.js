c5946d127a99c754d303b53b61967a6d8eff1c30
Kris <mfcc_kalman@slmail.me>
2025-05-09 01:06:54 +0000

    Maybe fixed init for real this time

--- Linkwarden/init.js
+++ Linkwarden/init.js
@@ -1,7 +1,8 @@
 const { spawn } = require('child_process');
 const commands = [
-	{ bin: '/nodejs/bin/node', args: ['packages/prisma/node_modules/.bin/prisma', 'migrate', 'deploy'] },
+	{ bin: '/nodejs/bin/node', args: ['packages/prisma/node_modules/.bin/prisma', 'migrate', 'deploy', '--schema', 'packages/prisma/schema.prisma'] },
 	{ bin: '/nodejs/bin/node', args: ['apps/web/node_modules/.bin/next', 'start'] },
+	{ bin: '/nodejs/bin/node', args: ['apps/worker/node_modules/.bin/tsx', 'apps/worker/worker.ts'] },
 ];
 
 const exec = (bin, args) => {
@@ -25,6 +26,7 @@ const start = async () => {
 		await exec(commands[0].bin, commands[0].args);
 		const nextProcess = spawn(commands[1].bin, commands[1].args, { stdio: 'inherit', detached: true });
 		nextProcess.unref();
+		await exec(commands[2].bin, commands[2].args);
 	} catch (error) {
 		console.error('Error:', error);
 	}
