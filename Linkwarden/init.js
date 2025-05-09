69d6e691cbbbdba00d5fb01203a45c005cfa5617
Kris <mfcc_kalman@slmail.me>
2025-05-09 00:57:52 +0000

    Testing modified init for linkwarden

--- Linkwarden/init.js
+++ Linkwarden/init.js
@@ -1,8 +1,7 @@
 const { spawn } = require('child_process');
 const commands = [
-	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/prisma', 'migrate', 'deploy'] },
-	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/next', 'start'] },
-	{ bin: '/nodejs/bin/node', args: ['node_modules/.bin/ts-node', '--transpile-only', '--skip-project', 'scripts/worker.ts'] }
+	{ bin: '/nodejs/bin/node', args: ['packages/prisma/node_modules/.bin/prisma', 'migrate', 'deploy'] },
+	{ bin: '/nodejs/bin/node', args: ['apps/web/node_modules/.bin/next', 'start'] },
 ];
 
 const exec = (bin, args) => {
@@ -26,7 +25,6 @@ const start = async () => {
 		await exec(commands[0].bin, commands[0].args);
 		const nextProcess = spawn(commands[1].bin, commands[1].args, { stdio: 'inherit', detached: true });
 		nextProcess.unref();
-		await exec(commands[2].bin, commands[2].args);
 	} catch (error) {
 		console.error('Error:', error);
 	}
