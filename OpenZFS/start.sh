c60a932359d0107a9de86f7510ac3a4df72d97ab
Kris <mfcc_kalman@slmail.me>
2026-05-04 21:03:22 +0000

    Switched from Fedora to Ubuntu for OpenZFS

--- OpenZFS/start.sh
+++ OpenZFS/start.sh
@@ -4,7 +4,7 @@ NAME=zfs-builder
 
 podman build -t $NAME .
 podman run --name $NAME --user 3005:3005 --security-opt no-new-privileges --cap-drop ALL --network none -d $NAME
-podman cp $NAME:/install/rpms.zip $HOME/Downloads/
+podman cp $NAME:/install/debs.zip $HOME/Downloads/
 podman stop $NAME
 podman rm $NAME -f
 podman image rm $NAME -f
